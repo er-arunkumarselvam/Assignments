dd83fd1a756bb2dc1226b9d44814a2ab26363052
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2022-10-11 18:09:14 +0530

    Two Class Created this program

---
+++ Assignment_1/Customer.java
@@ -0,0 +1,8 @@
+class Customer 
+{
+public static void main(String[] args) // Creation of Main Method
+{
+Theatre theatre = new Theatre(); // Object Creation
+theatre.bookTicket(200); // Method Calling another class - Theatre1
+}
+}
---
+++ Assignment_1/Theatre.java
@@ -0,0 +1,7 @@
+class Theatre 
+{
+void bookTicket(int amount) // Method Creation 
+{// Method Defination
+System.out.println("Balance is "+ (amount-120));
+}
+}
