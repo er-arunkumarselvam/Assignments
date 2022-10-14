eb885db96140395c44f9ab3bda43b7abbf152b36
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2022-10-14 20:36:36 +0530

    Assignment_0 -Learning return type and method-arguments passing

---
+++ Assignment_0/Theatre.java
@@ -0,0 +1,15 @@
+class Theatre 
+{
+public static void main(String[] args)
+{
+Theatre raja = new Theatre(); //Object Creation
+int balance_amount = raja.bookTicket(200); //Method Calling, receive and store data value of balance in balance_amount
+System.out.println("After booking ticket " + balance_amount); //Concatination Operator ("String"+value)
+}
+int bookTicket(int ticket) 
+{
+int ticket_price = 120;
+int balance = ticket-ticket_price;
+return balance; //return this value of balance
+}
+}
